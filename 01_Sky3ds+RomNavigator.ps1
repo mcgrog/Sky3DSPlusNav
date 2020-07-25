689da4f0f8c6c9e862262636aeb6f389bd0dd102
mcgrog <mcgrog@gmail.com>
2020-07-25 11:45:46 +1000

    First Commit, Wrote Readme

---
+++ 01_Sky3ds+RomNavigator.ps1
@@ -0,0 +1,68 @@
+﻿cls;$cd = $PSScriptRoot;if ($psise){$cd = ($psise.CurrentFile.FullPath | split-path -Parent);cls};cd $cd;$v=1.2
+while (!$Quit) {cls;start-sleep -milliseconds 100;
+    #region UI
+        "Sky3ds+ Navigation Aid $v" | Write-host -f Cyan -NoNewline;"`t`tBy Mcgrog [2020]" | Write-host -f DarkCyan;
+        "  Operation Instructions:`n`t - Enter`n`t`t- Get Directions`n`t - Type 'R' + Enter to:`n`t`t - Create New/Refresh '02_Games.xml'`n`t`t - Then Get Directions`n`t - Type 'Q' + Enter to:`n`t`t - Quit"|Write-host -f DarkGray
+        if ($LeftClicksMessage -or $RightClicksMessage) {
+            "`nShortest Path From: $($CurrentGame.Position) To: $($DestinationGame.Position) Of: $($Games.Count)" | Write-host -f Gray
+            $LeftClicksMessage | write-host -f Yellow
+            $RightClicksMessage | write-host -f Green
+        } elseif ($WarningMessage) {Write-Warning $WarningMessage}
+        $LeftClicksMessage = $RightClicksMessage = $WarningMessage = $null
+        $Input = Read-Host "`nType Modifier/Hit Enter to Continue"
+    #endregion
+    
+    switch -w ($Input) {
+        q {$Quit=$true;break}
+        r { # Get Rom List from FAT32 USB Device
+            $TargetDrive = @(gwmi Win32_logicaldisk | ? {$_.drivetype -eq 2 -and $_.filesystem -eq 'FAT32'} | select 'VolumeName','DeviceID',@{n='Capacity';e={($_.size/1gb).tostring('0.00 GB')}},@{n='Free Space';e={($_.freespace/1gb).tostring('0.00 GB')}},@{n='% Full';e={((1-($_.freespace/$_.size))*100).tostring('0.00')}} | ogv -t "Select Drive To Create New 02_Games.xml or Close To Use Existing 02_Games.xml" -PassThru)[0]
+            if($TargetDrive){
+                $Games = gci $TargetDrive.DeviceID | ? {$_.name -like "*.3ds"} | select * | sort Name
+                # Format with For Use In Navigation & Save File for later use
+                if($Games){
+                    $Games = $Games | select `
+                         @{n='Position'        ;e={[array]::indexof($Games,$_)+1}} `
+                        ,@{n='Name'            ;e={$_.BaseName}} `
+                        ,@{n='Trimmed'         ;e={$_.length % 0.125gb -ne 0}} `
+                        ,@{n='Size (MB)'       ;e={[int]$(($_.length/1MB).tostring('0'))}} `
+                        ,@{n='ConfigCreated'   ;e={test-path "$($TargetDrive.DeviceID)\$($_.BaseName).cfg"}} `
+                        ,@{n='SaveCreated'     ;e={test-path "$($TargetDrive.DeviceID)\$($_.BaseName).sav"}} `
+                        | select *, `
+                         @{n='Save Size (KB)'  ;e={if ($_.SaveCreated  ){((get-item "$($TargetDrive.DeviceID)\$($_.Name).sav" -ea ig).Length/1KB).tostring('0')}}} `
+                        ,@{n='Config Size (B)' ;e={if ($_.ConfigCreated){((get-item "$($TargetDrive.DeviceID)\$($_.Name).cfg" -ea ig).Length).tostring('0')}}} `
+                        ,@{n='CART_ID'         ;e={if ($_.ConfigCreated){((gc "$($TargetDrive.DeviceID)\$($_.Name).cfg").split("`r`n") | ? {$_ -like 'CART_ID*'     }).split('=')[1]}}} `
+                        ,@{n='ENC_TYPE'        ;e={if ($_.ConfigCreated){((gc "$($TargetDrive.DeviceID)\$($_.Name).cfg").split("`r`n") | ? {$_ -like 'ENC_TYPE*'    }).split('=')[1]}}} `
+                        ,@{n='ENC_SEED'        ;e={if ($_.ConfigCreated){((gc "$($TargetDrive.DeviceID)\$($_.Name).cfg").split("`r`n") | ? {$_ -like 'ENC_SEED*'    }).split('=')[1]}}} `
+                        ,@{n='FLASH_ID'        ;e={if ($_.ConfigCreated){((gc "$($TargetDrive.DeviceID)\$($_.Name).cfg").split("`r`n") | ? {$_ -like 'FLASH_ID*'    }).split('=')[1]}}} `
+                        ,@{n='GAMESAVE_KEY'    ;e={if ($_.ConfigCreated){((gc "$($TargetDrive.DeviceID)\$($_.Name).cfg").split("`r`n") | ? {$_ -like 'GAMESAVE_KEY*'}).split('=')[1]}}} 
+                    $Games | Export-Clixml .\02_Games.xml -Force
+                }
+            }
+        }
+        * { try{# Import Existing Data if None
+                if (!$Games){$Games = Import-Clixml .\02_Games.xml -ea stop}
+                #region Calculate Shortest Path Between Two Roms
+                    $CurrentGame     = @($Games | ogv -PassThru -t "Select Current Game")[0]
+                    if ($CurrentGame) {
+                        $DestinationGame = @($Games | ? {$_ -ne $CurrentGame} | ogv -PassThru -t "Select Destination - Current Game$CurrentGame")[0]
+                        if ($DestinationGame.Position -gt $CurrentGame.Position) {
+                            $LeftClicks  = $Games.Count - $DestinationGame.Position + $CurrentGame.Position
+                            $RightClicks = $DestinationGame.Position - $CurrentGame.Position
+                            switch((@($LeftClicks,$RightClicks) | Measure-Object -Minimum).minimum) {
+                                $LeftClicks  {$LeftClicksMessage = "$LeftClicks Left Clicks"}
+                                $RightClicks {$RightClicksMessage = "$RightClicks Right Clicks"}
+                            }
+                        } elseif ($DestinationGame.Position -lt $CurrentGame.Position) {
+                            $LeftClicks  = $CurrentGame.Position - $DestinationGame.Position
+                            $RightClicks = $Games.Count - $CurrentGame.Position + $DestinationGame.Position
+                            switch((@($LeftClicks,$RightClicks) | Measure-Object -Minimum).minimum) {
+                                $LeftClicks  {$LeftClicksMessage = "$LeftClicks Left Clicks"}
+                                $RightClicks {$RightClicksMessage = "$RightClicks Right Clicks"}
+                            }
+                        }
+                    }
+                #endregion
+            } catch {$WarningMessage = "No '02_Games.xml' Discovered"}
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Sky3DSPlusNav
-Quick Powershell Script for Navigating Sky3DS+ Library
+	By Mcgrog [2020]
+
+Quick Powershell Script for Navigating Sky3DS+ Library with simple outgrid view displaying rom details that allows marking your current rom and calculating the sortest path to a rom of your choosing via left and right buttons on your sky3ds+
+(Windows 10 will work, it may run on older versions of windows, i dont have anything with a wmf <5.1 anymore to test it on)
+
+#File 0)	00_EXECUTE_Sky3ds+RomNavigator.lnk
+ - Runs File 1 from explorer via double click.
+
+#File 1)	01_Sky3ds+RomNavigator.ps1
+ - On first run or any time a data refresh is required;from the console write R then press enter. (this bring up a menu selection of connected fat32 drives to scan for roms, upon selection the file 02_Games.xml is created, from here you can now remove your sd card and insert it into the sky3ds)
+ - On subsequent runs just press enter from the console. (this reads from the generated 02_Games.xml data file and displayed your library including per rom info and config.cfg information from the time of generation)
+ 
+ - The Navigation Screen:
+	This out grid view displays rom information discovered on last scan, you can navigate this with arrow keys, mouse, search bar, filters etc.
+	To calculate button presses required to navigate directly to the rom of your choice on your sky3ds:
+		1. Select the rom that is displayed on your 3DS.
+		2. Click Ok or Press Enter.
+			(selecting multiple will just result in the first rom being selected)
+		3. Select the rom you wish to navigate to from the roms that arent your first selection.
+		4. Click Ok or Press Enter.
+			(selecting multiple will just result in the first rom being selected)
+	
+		Shortest path will then be calculated using loopback logic and directions displayed in the console window.
+
+	Example:
+		If you have 50 roms, and you want to get from position 45 to position 5,
+		the shortest path is by continuing right past 50 by ten clicks as opposed to pressing left 40 times,
+		so the script will now display the amount of times pressing the right button can be done quickly without having to wait to for the sky3ds to load up all the roms inbetween.
+
+ - Q then Enter to Quit from the console screen
+
+#File 2)	02_Games.xml
+ - Generated whenever the sd card library is scanned.
+ - File is called after a library scan or on first launch if no game data exists in memory.
+
+Files can all be kept safely on your sky3ds+ sd card if you travel around and want to bring the navigator with you,
+Downside to this is needing to stick the sd card into a computer to launch the Navigator again so storing these files on a pc may prove more convenient,
+While the app is kept open from the first hit of enter everything is then stored in memory and sd card is no longer required,
+Data wont refresh from the card again unless you tell it to refresh with the R command.
+Data wont refresh from 02_Games.xml again after first Enter unless you tell it to refresh with the R command.
